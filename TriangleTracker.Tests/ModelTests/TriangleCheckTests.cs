04209591f23918d1811feff96538f1f8578c6ae2
William Donovan-Seid <wdstwo@live.com>
2020-09-22 10:48:04 -0700

    add all tests and pass them
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleCheckTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleCheckTests.cs
@@ -3,13 +3,35 @@ using TriangleCheck;
 
 namespace TriangleCheck.Tests
 {
+  [TestClass]
   public class TriangleCheckTests
   {
     [TestMethod]
-    public void CheckType_IsItATriangle_String()
+    public void CheckType_IsItATriangle_NoString()
     {
-      Triangle newTri = new Triangle(2,3,7);
-      Assert.AreEqual("", newTri.CheckType());
+      Triangle newTri = new Triangle(2,3,99);
+      Assert.AreEqual("not a triangle", newTri.CheckType());
+    }
+
+    [TestMethod]
+    public void CheckType_IsItATriangle_YesString()
+    {
+      Triangle newTri = new Triangle(2,3,4);
+      Assert.AreEqual("this is a triangle", newTri.CheckType());
+    }
+
+    [TestMethod]
+    public void CheckType_IsEqualTri_String()
+    {
+      Triangle newTri = new Triangle(3,3,3);
+      Assert.AreEqual("equalateral triangle", newTri.CheckType());
+    }
+
+    [TestMethod]
+    public void CheckType_IsIsoscelesTri_String()
+    {
+      Triangle newTri = new Triangle(3,3,5);
+      Assert.AreEqual("isosceles triangle", newTri.CheckType());
     }
   }
 }
